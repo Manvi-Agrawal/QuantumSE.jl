06a0d03fc8fd3cb91c61848d658d6b2a64f4bce7
Manvi-Agrawal <manvimeerut@gmail.com>
2024-08-25 11:48:35 -0700

    Separate stabilizer and neighbours

--- example/RotatedSC_mod_odd_d.jl
+++ example/RotatedSC_mod_odd_d.jl
@@ -111,78 +111,26 @@ end
     # print("Decoder end")
 end
 
-function get_stabilizer_and_nbr(d::Integer)
+function get_stabilizer(d::Integer, X_nbr, Z_nbr)
     num_qubits = d*d
-    gen_s = (num_qubits-1)÷2
 
     stabilizer = fill(false, num_qubits, 2*num_qubits)
 
-    X_nbr = [ [] for _ in 1:gen_s]
-    Z_nbr = [ [] for _ in 1:gen_s]
-
-    z_4q = [ 1 + r*d+c+r%2 for r in 0:(d-2) for c in 0:2:(d-2) ]
-    # println("Z 4q: $(z_4q)")
-
-    x_4q = [ 1 + r*d+c+(r+1)%2 for r in 0:(d-2) for c in 0:2:(d-2) ]
-    # println("X 4q: $(x_4q)")
-
-
-    x_2q_up = [ 1+c for c in 0:2:(d-2) ]
-    x_2q_down = [ 1+ (d-1)*d + c for c in 1:2:(d-2) ]
-    x_2q = vcat(x_2q_up, x_2q_down)
-
-#    println("X 2q: $(x_2q)")
-
-
-    z_2q_right = [ 1 + r*d+ (d-1) for r in 0:2:(d-2) ]
-    z_2q_left = [ 1 + r*d for r in 1:2:(d-2) ]
-    z_2q = vcat(z_2q_left, z_2q_right)
-
-#    println("Z 2q: $(z_2q)")
-
-    x_q = vcat(x_4q, x_2q)
-    z_q = vcat(z_4q, z_2q)
-
-
-
-    r = 0
-
-    for j in eachindex(x_4q)
-        for x_adj in  _adj4( d, x_4q[j])
-            stabilizer[r+j, x_adj] = true
-            push!(X_nbr[j], x_adj)
+    for row in 1:length(X_nbr)
+        for col in 1:length(X_nbr[row])
+            s_col = X_nbr[row][col]
+            # println("Set $(row), $(s_col) to true...")
+            stabilizer[row, s_col] = true
         end
     end
-
-    r += length(x_4q)
-
-
-    for j in eachindex(x_2q)
-        for x_adj in _adj2_hor( d, x_2q[j])
-            stabilizer[r+j, x_adj] = true
-            # println("Push X_nbr[$(length(x_4q) + j)] <- $(x_adj)")
-
-            push!(X_nbr[length(x_4q) + j], x_adj)
+    
+    for row in 1 : length(Z_nbr)
+        for col in 1:length(Z_nbr[row])
+            s_row = row + length(X_nbr)
+            s_col = num_qubits+Z_nbr[row][col]
+            stabilizer[s_row, s_col] = true
         end
-    end
-
-    r += length(x_2q)
-
-    for j in eachindex(z_4q)
-        for z_adj in _adj4( d, z_4q[j])
-            stabilizer[r+j, num_qubits+z_adj] = true
-            push!(Z_nbr[j], z_adj)
-        end
-    end
-
-    r += length(z_4q)
-
-    for j in eachindex(z_2q)
-        for z_adj in _adj2_ver( d, z_2q[j])
-            stabilizer[r+j, num_qubits+z_adj] = true
-            push!(Z_nbr[length(z_4q) + j], z_adj)
-        end
-    end
+    end 
 
     # r: 1, 2, ..., d ;; c = 1
     sc_lx = [ 1+ r*d + 1 for r in 0:(d-1) ]
@@ -191,7 +139,66 @@ function get_stabilizer_and_nbr(d::Integer)
         stabilizer[num_qubits, idx] = true
     end
 
-    return (stabilizer, X_nbr, Z_nbr)
+    return stabilizer
+
+end
+
+function get_nbr(d::Integer)
+    num_qubits = d*d
+    gen_s = (num_qubits-1)÷2
+
+    X_nbr = [ [] for _ in 1:gen_s]
+    Z_nbr = [ [] for _ in 1:gen_s]
+
+    z_4q = [ 1 + r*d+c+r%2 for r in 0:(d-2) for c in 0:2:(d-2) ]
+
+    x_4q = [ 1 + r*d+c+(r+1)%2 for r in 0:(d-2) for c in 0:2:(d-2) ]
+
+
+    x_2q_up = [ 1+c for c in 0:2:(d-2) ]
+    x_2q_down = [ 1+ (d-1)*d + c for c in 1:2:(d-2) ]
+    x_2q = vcat(x_2q_up, x_2q_down)
+
+
+
+    z_2q_right = [ 1 + r*d+ (d-1) for r in 0:2:(d-2) ]
+    z_2q_left = [ 1 + r*d for r in 1:2:(d-2) ]
+    z_2q = vcat(z_2q_left, z_2q_right)
+
+
+    x_q = vcat(x_4q, x_2q)
+    z_q = vcat(z_4q, z_2q)
+
+
+    for j in eachindex(x_4q)
+        for x_adj in  _adj4( d, x_4q[j])
+            push!(X_nbr[j], x_adj)
+        end
+    end
+
+    for j in eachindex(x_2q)
+        for x_adj in _adj2_hor( d, x_2q[j])
+            push!(X_nbr[length(x_4q) + j], x_adj)
+        end
+    end
+
+    # r += length(x_2q)
+
+    for j in eachindex(z_4q)
+        for z_adj in _adj4( d, z_4q[j])
+            push!(Z_nbr[j], z_adj)
+        end
+    end
+
+    for j in eachindex(z_2q)
+        for z_adj in _adj2_ver( d, z_2q[j])
+            push!(Z_nbr[length(z_4q) + j], z_adj)
+        end
+    end
+
+    
+
+    return (X_nbr, Z_nbr)
 
 end
 
@@ -241,14 +248,17 @@ function check_surface_code_decoder(d::Integer)
     @info "Initialization Stage"
     t0 = time()
     begin
-        (stabilizer, X_nbr, Z_nbr) = get_stabilizer_and_nbr(d)
+        ( X_nbr, Z_nbr) = get_nbr(d)
+        stabilizer = get_stabilizer(d, X_nbr, Z_nbr)
+
         phases = get_phases(d)
     end
 
-    # println("Encoded stabilizer : $(stabilizer)")
-
+    
     # println("X idxs: $(X_nbr)")
-    # print("Z idxs: $(Z_nbr)")
+    # println("Z idxs: $(Z_nbr)")
+
+    # println("Encoded stabilizer : $(stabilizer)")
 
     @info "Configuration Init"
     t1 = time()
@@ -291,7 +301,7 @@ check_surface_code_decoder(3) # precompile time
 open("surface_code.csv", "w") do io
     println(io, "d,res,nq,all,init,config,cons_gen,cons_sol")
 
-    for d in 23:2:29
+    for d in 3:3
         res_d, all, init, config, cons_gen, cons_sol = check_surface_code_decoder(d)
         println("d,res,nq,all,init,config,cons_gen,cons_sol")
         println("$(d),$(res_d),$(d*d),$(all),$(init),$(config),$(cons_sol),$(cons_gen)")
