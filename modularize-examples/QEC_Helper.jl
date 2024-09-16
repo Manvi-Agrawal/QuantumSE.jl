f3ea6a686e1288649428550e55a2b4941222f099
Manvi-Agrawal <manvimeerut@gmail.com>
2024-09-16 14:32:36 -0700

    Improvise get_stabilizer_from_nbr function

--- modularize-examples/QEC_Helper.jl
+++ modularize-examples/QEC_Helper.jl
@@ -7,17 +7,17 @@ function get_stabilizer_from_nbr(num_qubits::Integer, X_nbr, Z_nbr, l_op)
     stabilizer = fill(false, num_qubits, 2*num_qubits)
 
     for row in 1:length(X_nbr)
-        for col in 1:length(X_nbr[row])
-            s_col = X_nbr[row][col]
-            # println("Set $(row), $(s_col) to true...")
-            stabilizer[row, s_col] = true
+        for idx in X_nbr[row]
+            # println("Set $(row), $(idx) to true...")
+            stabilizer[row, idx] = true
         end
     end
     
     for row in 1 : length(Z_nbr)
-        for col in 1:length(Z_nbr[row])
+        for idx in Z_nbr[row]
             s_row = row + length(X_nbr)
-            s_col = num_qubits+Z_nbr[row][col]
+            s_col = num_qubits+idx
+            # println("Set $(s_row), $(s_col) to true...")
             stabilizer[s_row, s_col] = true
         end
     end 
