6a559998bb48aca682882e6f5b9d5740549484f8
Manvi-Agrawal <manvimeerut@gmail.com>
2024-09-16 01:35:26 -0700

    WIP

--- modularize-examples/ToricCode.jl
+++ modularize-examples/ToricCode.jl
@@ -45,22 +45,6 @@ function toric_x_s(d::Integer, idx::Integer)
 	s
 end
 
-@qprog toric_x_m (d, idx) begin
-    b = _xadj(d, idx)
-    
-    CNOT(b[1], b[2])
-    CNOT(b[3], b[4])
-    CNOT(b[3], b[1])
-    H(b[3])
-    res = M(b[3])
-    H(b[3])
-    CNOT(b[3], b[1])
-    CNOT(b[3], b[4])
-    CNOT(b[1], b[2])
-    
-    res
-end
-
 function toric_z_s(d::Integer, idx::Integer)
 	s = zeros(Bool, 4*d*d)
 
@@ -69,20 +53,6 @@ function toric_z_s(d::Integer, idx::Integer)
 	end
 
 	s
-end
-
-@qprog toric_z_m (d, idx) begin
-    b = _zadj(d, idx)
-    
-    CNOT(b[1], b[2])
-    CNOT(b[3], b[4])
-    CNOT(b[2], b[4])
-    res = M(b[4])
-    CNOT(b[2], b[4])
-    CNOT(b[3], b[4])
-    CNOT(b[1], b[2])
-    
-    res
 end
 
 function toric_lx1(d::Integer)
@@ -125,141 +95,91 @@ function toric_lz2(d::Integer)
 	s
 end
 
+@qprog toric_x_m (d, idx) begin
+    b = _xadj(d, idx)
+    
+    CNOT(b[1], b[2])
+    CNOT(b[3], b[4])
+    CNOT(b[3], b[1])
+    H(b[3])
+    res = M(b[3])
+    H(b[3])
+    CNOT(b[3], b[1])
+    CNOT(b[3], b[4])
+    CNOT(b[1], b[2])
+    
+    res
+end
 
 
-function check_toric_decoder(d::Integer)
 
-    @info "Initailization Stage"
-    t0 = time()
-    begin
-        num_qubits = d*d*2
+@qprog toric_z_m (d, idx) begin
+    b = _zadj(d, idx)
+    
+    CNOT(b[1], b[2])
+    CNOT(b[3], b[4])
+    CNOT(b[2], b[4])
+    res = M(b[4])
+    CNOT(b[2], b[4])
+    CNOT(b[3], b[4])
+    CNOT(b[1], b[2])
+    
+    res
+end
 
-	    stabilizer = Matrix{Bool}(undef, num_qubits, 2*num_qubits)
-	    phases = Vector{Z3.ExprAllocated}(undef, num_qubits)
+
+
+function get_phases(d)
+    phases = Vector{Z3.ExprAllocated}(undef, num_qubits)
 	    lx = _bv_const(ctx, "lx")
 	    lz = _bv_const(ctx, "lz")
-
-	    @simd for i in 1:d*d-1
-	    	stabilizer[i,:] = toric_x_s(d, i)
-	    	stabilizer[i+d*d,:] = toric_z_s(d, i)
+        
+	    for i in 1:d*d-1
 	    	phases[i] = _bv_val(ctx, 0)
 	    	phases[i+d*d] = _bv_val(ctx, 0)
 	    end
 
-	    stabilizer[d*d,:] = toric_lx1(d)
 	    phases[d*d] = lx
-	    stabilizer[2*d*d,:] = toric_lz1(d)
 	    phases[2*d*d] = lz
-
-        ρ01 = from_stabilizer(num_qubits, stabilizer, phases, ctx)
-        ρ1 = copy(ρ01)
-
-        stabilizer[d*d,:] = toric_lx2(d)
-	    phases[d*d] = lx
-	    stabilizer[2*d*d,:] = toric_lz2(d)
-	    phases[2*d*d] = lz
-
-        ρ02 = from_stabilizer(num_qubits, stabilizer, phases, ctx)
-        ρ2 = copy(ρ02)
-
-        σ = CState([(:d, d),
-            (:toric_decoder, toric_decoder),
-            (:toric_z_m, toric_z_m),
-            (:toric_x_m, toric_x_m),
-            (:_xadj, _xadj),
-            (:_zadj, _zadj),
-            (:ctx, ctx),
-            (:mwpm, mwpm)
-        ])
-
-        num_x_errors = (d-1)÷2
-        x_errors = inject_errors(ρ1, "X")
-        ϕ_x1 = _sum(ctx, x_errors, num_qubits) == bv_val(ctx, num_x_errors, _len2(num_qubits)+1)
-
-	    #num_z_errors = (d-1)÷2
-        #z_errors = inject_errors(ρ1, "Z")
-        #ϕ_z1 = _sum(ctx, z_errors, num_qubits) == bv_val(ctx, num_z_errors, _len2(num_qubits)+1)
-
-        x_errors = inject_errors(ρ2, "X")
-        ϕ_x2 = _sum(ctx, x_errors, num_qubits) == bv_val(ctx, num_x_errors, _len2(num_qubits)+1)
-
-	    #num_z_errors = (d-1)÷2
-        #z_errors = inject_errors(ρ2, "Z")
-        #ϕ_z2 = _sum(ctx, z_errors, num_qubits) == bv_val(ctx, num_z_errors, _len2(num_qubits)+1)
-
-        cfg1 = SymConfig(toric_decoder(d), σ, ρ1)
-        cfg2 = SymConfig(toric_decoder(d), σ, ρ2)
-    end
-
-    @info "Symbolic Execution Stage"
-    t1 = time()
-    begin
-        cfgs1 = QuantSymEx(cfg1)
-        cfgs2 = QuantSymEx(cfg2)
-    end
-
-    @info "SMT Solver Stage"
-    t2 = time()
-    begin
-        res = true
-        for cfg in cfgs1
-            if !check_state_equivalence(
-                cfg.ρ, ρ01, (ϕ_x1 #=& ϕ_z1=#, cfg.ϕ[1], cfg.ϕ[2]),
-                `bitwuzla --smt-comp-mode true -rwl 0 -S kissat`
-                #`bitwuzla --smt-comp-mode true -S kissat`
-                #`bitwuzla --smt-comp-mode true -rwl 0`
-               )
-                res = false
-                break
-            end
-        end
-
-        if res
-            for cfg in cfgs2
-                if !check_state_equivalence(
-                    cfg.ρ, ρ02, (ϕ_x2 #=& ϕ_z2=#, cfg.ϕ[1], cfg.ϕ[2]),
-                    `bitwuzla --smt-comp-mode true -rwl 0 -S kissat`
-                    #`bitwuzla --smt-comp-mode true -S kissat`
-                    #`bitwuzla --smt-comp-mode true -rwl 0`
-                   )
-                    res = false
-                    break
-                end
-            end
-        end
-    end
-
-    t3 = time()
-
-    res, t3-t0, t1-t0, t2-t1, t3-t2
 end
 
-check_toric_decoder(3) # precompile time
+function get_stabilizer(d)
+    stabilizer = Matrix{Bool}(undef, num_qubits, 2*num_qubits)
 
-open("toric_code.dat", "w") do io
-  println(io, "nq all init qse smt")
-  println("nq all init qse smt")
-  for d in 4:27
-    res, all, init, qse, smt = check_toric_decoder(d)
-    println(io, "$(d*d*2) $(all) $(init) $(qse) $(smt)")
-    println("$(d)/27: $(d*d*2) $(all) $(init) $(qse) $(smt)")
-  end
+    for i in 1:d*d-1
+        stabilizer[i,:] = toric_x_s(d, i)
+        stabilizer[i+d*d,:] = toric_z_s(d, i)
+    end
+
+    stabilizer[d*d,:] = toric_lx1(d)
+    stabilizer[2*d*d,:] = toric_lz1(d)
 end
 
-open("rsc.csv", "w") do io
+
+
+open("toric_code.csv", "w") do io
     println(io, "d,res,nq,all,init,config,cons_gen,cons_sol")
 
-    for d in 4:15
+    for d in 3:20
         tm2 = time()
-        (X_nbr, Z_nbr) = get_nbr(d)
+        # (X_nbr, Z_nbr) = get_nbr(d)
+
+        stabilizer = QEC_Helper.get_stabilizer(d, X_nbr, Z_nbr)
+
         
-        rsc_decoder = QEC_Pipeline.QecDecoderConfig(
+        toric_decoder = QEC_Pipeline.QecDecoderConfig(
             d=d,
             X_nbr=X_nbr,
             Z_nbr=Z_nbr,
+            stabilizer=stabilizer,
             phases= get_phases(d),
             ctx=ctx,
             bug = rsc_bug)
+
+        toric_decoder.x_syndrome_circuit = toric_x_m
+        toric_decoder.z_syndrome_circuit = toric_z_m
+        toric_decoder.decoder_algo_xz = mwpm
+
 
         tm1 = time()
         
@@ -269,14 +189,13 @@ open("rsc.csv", "w") do io
 
         # println("Phases: $(phases)")
 
-        res_d, all, init, config, cons_gen, cons_sol = QEC_Pipeline.check_qec_decoder(rsc_decoder)
+        res_d, all, init, config, cons_gen, cons_sol = QEC_Pipeline.check_qec_decoder(toric_decoder)
 
         init_config = (tm2-tm1)
-        # all += init
+        all += init_config
 
-        println("d,res,nq,all,init,config,cons_gen,cons_sol")
-        println("$(d),$(res_d),$(d*d),$(all),$(init),$(config),$(cons_gen),$(cons_sol)")
-        println(io, "$(d),$(res_d),$(d*d),$(all),$(init),$(config),$(cons_gen),$(cons_sol)")
+        println("d,res,nq,all,init_config, init,config,cons_gen,cons_sol")
+        println("$(d),$(res_d),$(2*d*d),$(all),$(init_config),$(init),$(config),$(cons_gen),$(cons_sol)")
+        println(io, "$(d),$(res_d),$(2*d*d),$(all),$(init_config),$(init),$(config),$(cons_gen),$(cons_sol)")
     end
 end
-
